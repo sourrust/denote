162f3870896ee7b4648a39c87b59e957d4b94dbd
Jeremy Hull <sourdrums@gmail.com>
2013-11-10 02:49:39 -0700

    Change model for new data coming in

--- js/models/note.js
+++ js/models/note.js
@@ -6,9 +6,17 @@ function(Backbone) {
   return Backbone.Model.extend({
     defaults: {
       'preview_text': '',
+      'full_text': '',
       'permalink': '',
       'classes': [],
-      'blogs': []
+      'blogs': [],
+      'is_preview': true
+    },
+
+    togglePreview: function() {
+      var isPreview = this.get('is_preview');
+
+      this.set('is_preview', !isPreview);
     }
   });
 });
