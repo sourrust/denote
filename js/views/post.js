8e7883961295b39703eb5f4f669c2374e29e4bf7
Jeremy Hull <sourdrums@gmail.com>
2013-11-10 02:51:54 -0700

    Add view + html for full post commentary display

---
+++ js/views/post.js
@@ -0,0 +1,23 @@
+define('views/post', ['underscore', 'backbone'],
+
+function(_, Backbone) {
+  'use strict';
+
+  return Backbone.View.extend({
+    el: '.post-container',
+
+    initialize: function() {
+      _.bindAll(this, 'render');
+
+      this.model.on('change', this.render);
+    },
+
+    render: function() {
+      var text = this.model.get('full_text');
+
+      this.$el.html(text);
+
+      return this;
+    }
+  });
+});
