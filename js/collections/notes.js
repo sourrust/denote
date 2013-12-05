ba27ece4432a7701be15a8395e63d6e4a32ecb39
Jeremy Hull <sourdrums@gmail.com>
2013-12-04 17:08:45 -0700

    Move findOffset over to NotesCollection

--- js/collections/notes.js
+++ js/collections/notes.js
@@ -3,6 +3,18 @@ define('collections/notes', ['backbone', 'models/note'],
 function(Backbone, NoteModel) {
   'use strict';
 
+  var findOffset = function(context) {
+    var $moreNotes, offset;
+
+    $moreNotes = context.find('.more_notes_link');
+
+    if(!$moreNotes.length) return;
+
+    offset = $moreNotes.attr('onclick').match(/\?from_c=\d+/)[0];
+
+    return offset;
+  };
+
   return Backbone.Collection.extend({
     model: NoteModel
   });
--- js/utility.js
+++ js/utility.js
@@ -47,18 +47,6 @@ function(_, $) {
     $el.removeClass(exchange);
   };
 
-  utility.findOffset = function(context) {
-    var $moreNotes, offset;
-
-    $moreNotes = context.find('.more_notes_link');
-
-    if(!$moreNotes.length) return;
-
-    offset = $moreNotes.attr('onclick').match(/\?from_c=\d+/)[0];
-
-    return offset;
-  };
-
   utility.canGrabMoreNotes = function(notesJSON, context) {
     var correctLength, endOfNotes;
 
