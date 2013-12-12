d2ca932c69bc4bf5fe7d9bdd1843fa288ebf7f10
Jeremy Hull <sourdrums@gmail.com>
2013-12-11 18:07:07 -0700

    Copy over bower packages for default grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,6 +99,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['less','jst','jshint']);
+  grunt.registerTask('default', ['less','jst','copy:bower','jshint']);
   grunt.registerTask('release', ['less','jst', 'copy']);
 };
