9fdb7868863ce6a6e7ce419bd453d840a894a7d4
Jeremy Hull <sourdrums@gmail.com>
2014-06-30 19:43:34 -0700

    Parsing in other response post for note model
    This will make it easier to see the context of the post the user clicked on
    and follow the conversation all the way down.

--- js/models/note.js
+++ js/models/note.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var _        = require('underscore'),
+    $        = require('jquery'),
     Backbone = require('backbone');
 
 module.exports = Backbone.Model.extend({
@@ -30,18 +31,28 @@ module.exports = Backbone.Model.extend({
   },
 
   parse: function(response, options) {
-    var body, content, post;
+    var $blogs, $content, $body, body, content, post;
 
     // Parse function in model gets called when the collection fetch
     // method gets called. Simply returns the model that has already been
     // parsed because we are looking for tumblr api reponses to parse.
     if(options.dataType) return response;
 
-    post    = response.response.posts[0];
-    body    = post.body || post.description || post.caption;
-    content = _.last(body.split('</blockquote>'));
+    post = response.response.posts[0];
+    body = post.body || post.description || post.caption;
 
-    return { 'full_text': content.trim() };
+    // Appending a container onto the body of the post contents allows
+    // jQuery to find top level tags. This is mainly for finding blockquote
+    // tags because when there is only one response, and one top level
+    // blockquote, jQuery couldn't find that tag.
+    $body    = $('<div></div>').append(body);
+    content  = _.last(body.split('</blockquote>'));
+    $blogs   = $body.find('.tumblr_blog');
+    $content = $body.find('blockquote');
+
+    return { 'full_text': content.trim()
+           , 'responses': getResponses($blogs, $content)
+           };
   },
 
   togglePreview: function() {
