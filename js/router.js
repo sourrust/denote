b4dbcb8f77ef43c943db0ce513b2d9e7dce32f8b
Jeremy Hull <sourdrums@gmail.com>
2014-09-26 16:22:12 -0700

    Use Router object to handle switching from views

---
+++ js/router.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var Backbone  = require('backbone'),
+    Notes     = require('collections/notes'),
+    NotesView = require('views/notecontainer'),
+    PostView  = require('views/post'),
+    utility   = require('utility');
+
+var collection, views = {};
+
+module.exports = Backbone.Router.extend({
+  routes: {
+    'index': 'noteContainer',
+    'post/:id': 'fullPost'
+  },
+
+  initialize: function(options) {
+    collection = new Notes();
+    collection.storeInitialData(options.data);
+
+    views.notes = new NotesView({
+      collection: collection,
+      router: this
+    });
+
+    views.fullPost = new PostView({ router: this });
+  },
+
+  noteContainer: function() {
+    utility.swapClass(views.notes.$el, 'hide', 'show');
+    utility.swapClass(views.fullPost.$el, 'show', 'hide');
+  },
+
+  fullPost: function(id) {
+    var oldView = views.notes,
+        newView = views.fullPost;
+
+    newView.model = collection.get(id);
+    newView.render();
+
+    utility.swapClass(oldView.$el, 'show', 'hide');
+    utility.swapClass(newView.$el, 'hide', 'show');
+  }
+});
