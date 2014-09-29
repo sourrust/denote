2ac528fc981809c89af44ea74991c9aabfa1d574
Jeremy Hull <sourdrums@gmail.com>
2014-09-28 22:15:56 -0700

    Change findOffset to named function
    Keep the function defining consistent. This also changes the context
    variable to represent that it is a jQuery object.

--- js/collections/notes.js
+++ js/collections/notes.js
@@ -5,17 +5,17 @@ var Backbone  = require('backbone');
 var NoteModel = require('models/note');
 var utility   = require('utility');
 
-var findOffset = function(context) {
+function findOffset($context) {
   var $moreNotes, offset;
 
-  $moreNotes = context.find('.more_notes_link');
+  $moreNotes = $context.find('.more_notes_link');
 
   if(!$moreNotes.length) return;
 
   offset = $moreNotes.attr('onclick').match(/\?from_c=\d+/)[0];
 
   return offset;
-};
+}
 
 module.exports = Backbone.Collection.extend({
   model: NoteModel,
